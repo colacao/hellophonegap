58e4a94e3bcd1870b8522d20943a9a8071b10c31
colacao <caoyueqq@163.com>
2012-09-15 00:10:58 +0800

    ff

--- src/com/ycao/HellophonegapActivity.java
+++ src/com/ycao/HellophonegapActivity.java
@@ -7,7 +7,13 @@ public class HellophonegapActivity extends DroidGap {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
+       
+    	
         super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/index.html");
+       // this.appView.setBackgroundColor(0);
+       // this.appView.setBackgroundResource(R.drawable.logo);
+        super.setIntegerProperty("splashscreen",R.drawable.bg2);
+        super.loadUrl("file:///android_asset/www/index.html",1500);
+      
     }
 }
